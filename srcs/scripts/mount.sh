da2c62723237f2a0b9da3a75dcb3d094b4f2be6b
Tanawat Jukmongkol <tjukmong@z2t4c2.42bangkok.com>
2025-04-04 18:00:51 +0700

    fixed mounting and entry script

--- srcs/scripts/mount.sh
+++ srcs/scripts/mount.sh
@@ -1,4 +1,6 @@
 
 echo "Mounting $LFS_IMG..."
+mkdir -p $LFS && \
 guestmount -a $LFS_IMG -m /dev/sda4:/ -m /dev/sda2:/boot -m /dev/sda1:/boot/efi --rw $LFS && \
+chown -R lfs:lfs $LFS && \
 echo "Mount successfully!"
