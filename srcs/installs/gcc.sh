f944a2c5b6d5d1ee2a72aeececd19a44e19a2b36
Tanawat Jukmongkol <tjukmong@z2t18c1.42bangkok.com>
2025-04-06 01:50:41 +0700

    add other packages back

--- srcs/installs/gcc.sh
+++ srcs/installs/gcc.sh
@@ -7,15 +7,15 @@ mkdir -p $BUILD_DIR
 # Wget
 # Binutils-2.32
 wget http://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.xz --continue --directory-prefix=$SRC_DIR &
-# # GCC-8.2.0
-# wget http://www.mpfr.org/mpfr-4.0.2/mpfr-4.0.2.tar.xz --continue --directory-prefix=$SRC_DIR &
-# wget http://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.xz --continue --directory-prefix=$SRC_DIR &
-# wget https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz --continue --directory-prefix=$SRC_DIR &
-# wget http://ftp.gnu.org/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.xz --continue --directory-prefix=$SRC_DIR &
-# # Linux-4.20.12
-# wget https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.20.12.tar.xz --continue --directory-prefix=$SRC_DIR &
-# # Glibc-2.29 
-# wget http://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.xz --continue --directory-prefix=$SRC_DIR &
+# GCC-8.2.0
+wget http://www.mpfr.org/mpfr-4.0.2/mpfr-4.0.2.tar.xz --continue --directory-prefix=$SRC_DIR &
+wget http://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.xz --continue --directory-prefix=$SRC_DIR &
+wget https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz --continue --directory-prefix=$SRC_DIR &
+wget http://ftp.gnu.org/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.xz --continue --directory-prefix=$SRC_DIR &
+# Linux-4.20.12
+wget https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.20.12.tar.xz --continue --directory-prefix=$SRC_DIR &
+# Glibc-2.29 
+wget http://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.xz --continue --directory-prefix=$SRC_DIR &
 
 wait
 
@@ -90,211 +90,211 @@ make -j$BUILD_JOBS $MAKE_FLAGS install
 
 EOF
 
-# # GCC-8.2.0
+# GCC-8.2.0
 
-# status=status&&install_package gcc-8.2.0.tar.xz 1 << EOF
+status=status&&install_package gcc-8.2.0.tar.xz 1 << EOF
 
-# tar -xf $SRC_DIR/mpfr-4.0.2.tar.xz
-# mv -v mpfr-4.0.2 mpfr
-# tar -xf $SRC_DIR/gmp-6.1.2.tar.xz
-# mv -v gmp-6.1.2 gmp
-# tar -xf $SRC_DIR/mpc-1.1.0.tar.gz
-# mv -v mpc-1.1.0 mpc
+tar -xf $SRC_DIR/mpfr-4.0.2.tar.xz
+mv -v mpfr-4.0.2 mpfr
+tar -xf $SRC_DIR/gmp-6.1.2.tar.xz
+mv -v gmp-6.1.2 gmp
+tar -xf $SRC_DIR/mpc-1.1.0.tar.gz
+mv -v mpc-1.1.0 mpc
 
-# for file in gcc/config/{linux,i386/linux{,64}}.h
-# do
-#   cp -uv \$file{,.orig}
-#   sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
-#       -e 's@/usr@/tools@g' \$file.orig > \$file
-#   echo '
-# #undef STANDARD_STARTFILE_PREFIX_1
-# #undef STANDARD_STARTFILE_PREFIX_2
-# #define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
-# #define STANDARD_STARTFILE_PREFIX_2 ""' >> \$file
-#   touch \$file.orig
-# done
+for file in gcc/config/{linux,i386/linux{,64}}.h
+do
+  cp -uv \$file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+      -e 's@/usr@/tools@g' \$file.orig > \$file
+  echo '
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+#define STANDARD_STARTFILE_PREFIX_2 ""' >> \$file
+  touch \$file.orig
+done
 
-# case $(uname -m) in
-#   x86_64)
-#     sed -e '/m64=/s/lib64/lib/' \
-#         -i.orig gcc/config/i386/t-linux64
-#  ;;
-# esac
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+ ;;
+esac
 
-# mkdir -v build
-# cd       build
+mkdir -v build
+cd       build
 
-# ../configure                                       \
-#     --target=$LFS_TGT                              \
-#     --prefix=/tools                                \
-#     --with-glibc-version=2.11                      \
-#     --with-sysroot=$LFS                            \
-#     --with-newlib                                  \
-#     --without-headers                              \
-#     --with-local-prefix=/tools                     \
-#     --with-native-system-header-dir=/tools/include \
-#     --disable-nls                                  \
-#     --disable-shared                               \
-#     --disable-multilib                             \
-#     --disable-decimal-float                        \
-#     --disable-threads                              \
-#     --disable-libatomic                            \
-#     --disable-libgomp                              \
-#     --disable-libmpx                               \
-#     --disable-libquadmath                          \
-#     --disable-libssp                               \
-#     --disable-libvtv                               \
-#     --disable-libstdcxx                            \
-#     --enable-languages=c,c++
+../configure                                       \
+    --target=$LFS_TGT                              \
+    --prefix=/tools                                \
+    --with-glibc-version=2.11                      \
+    --with-sysroot=$LFS                            \
+    --with-newlib                                  \
+    --without-headers                              \
+    --with-local-prefix=/tools                     \
+    --with-native-system-header-dir=/tools/include \
+    --disable-nls                                  \
+    --disable-shared                               \
+    --disable-multilib                             \
+    --disable-decimal-float                        \
+    --disable-threads                              \
+    --disable-libatomic                            \
+    --disable-libgomp                              \
+    --disable-libmpx                               \
+    --disable-libquadmath                          \
+    --disable-libssp                               \
+    --disable-libvtv                               \
+    --disable-libstdcxx                            \
+    --enable-languages=c,c++
 
-# make -j$BUILD_JOBS $MAKE_FLAGS
-# make -j$BUILD_JOBS $MAKE_FLAGS install
+make -j$BUILD_JOBS $MAKE_FLAGS
+make -j$BUILD_JOBS $MAKE_FLAGS install
 
-# EOF
+EOF
 
-# # Linux-4.20.12
+# Linux-4.20.12
 
-# install_package linux-4.20.12.tar.xz << EOF
+install_package linux-4.20.12.tar.xz << EOF
 
-# make mrproper
+make mrproper
 
-# make INSTALL_HDR_PATH=dest \
-#      -j$BUILD_JOBS $MAKE_FLAGS \
-#      headers_install
+make INSTALL_HDR_PATH=dest \
+     -j$BUILD_JOBS $MAKE_FLAGS \
+     headers_install
 
-# cp -rv dest/include/* /tools/include
+cp -rv dest/include/* /tools/include
 
-# EOF
+EOF
 
-# # Glibc-2.29
+# Glibc-2.29
 
-# install_package glibc-2.29.tar.xz << EOF
+install_package glibc-2.29.tar.xz << EOF
 
-# mkdir -v build
-# cd       build
+mkdir -v build
+cd       build
 
-# ../configure                             \
-#       --prefix=/tools                    \
-#       --host=$LFS_TGT                    \
-#       --build=$(../scripts/config.guess) \
-#       --enable-kernel=3.2                \
-#       --with-headers=/tools/include
+../configure                             \
+      --prefix=/tools                    \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --enable-kernel=3.2                \
+      --with-headers=/tools/include
 
-# make -j$BUILD_JOBS $MAKE_FLAGS
-# make -j$BUILD_JOBS $MAKE_FLAGS install
+make -j$BUILD_JOBS $MAKE_FLAGS
+make -j$BUILD_JOBS $MAKE_FLAGS install
 
-# # echo 'int main(){}' > dummy.c
-# # $LFS_TGT-gcc dummy.c
-# # readelf -l a.out | grep ': /tools'
-# # rm -v dummy.c a.out
+# echo 'int main(){}' > dummy.c
+# $LFS_TGT-gcc dummy.c
+# readelf -l a.out | grep ': /tools'
+# rm -v dummy.c a.out
 
-# EOF
+EOF
 
-# # Libstdc++ from GCC-8.2.0
+# Libstdc++ from GCC-8.2.0
 
-# install_package gcc-8.2.0.tar.xz 2 << EOF
+install_package gcc-8.2.0.tar.xz 2 << EOF
 
-# cd       build
-# rm       ./config.cache
+cd       build
+rm       ./config.cache
 
-# ../libstdc++-v3/configure           \
-#     --host=$LFS_TGT                 \
-#     --prefix=/tools                 \
-#     --disable-multilib              \
-#     --disable-nls                   \
-#     --disable-libstdcxx-threads     \
-#     --disable-libstdcxx-pch         \
-#     --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/8.2.0
+../libstdc++-v3/configure           \
+    --host=$LFS_TGT                 \
+    --prefix=/tools                 \
+    --disable-multilib              \
+    --disable-nls                   \
+    --disable-libstdcxx-threads     \
+    --disable-libstdcxx-pch         \
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/8.2.0
 
-# make -j$BUILD_JOBS $MAKE_FLAGS
-# make -j$BUILD_JOBS $MAKE_FLAGS install
+make -j$BUILD_JOBS $MAKE_FLAGS
+make -j$BUILD_JOBS $MAKE_FLAGS install
 
-# EOF
+EOF
 
-# # Pass 2
-# # Binutils-2.32 - Pass 2
+# Pass 2
+# Binutils-2.32 - Pass 2
 
-# install_package binutils-2.32.tar.xz 2 << EOF
+install_package binutils-2.32.tar.xz 2 << EOF
 
-# mkdir -v build
-# cd       build
+mkdir -v build
+cd       build
 
-# CC=$LFS_TGT-gcc                \
-# AR=$LFS_TGT-ar                 \
-# RANLIB=$LFS_TGT-ranlib         \
-# ../configure                   \
-#     --prefix=/tools            \
-#     --disable-nls              \
-#     --disable-werror           \
-#     --with-lib-path=/tools/lib \
-#     --with-sysroot
+CC=$LFS_TGT-gcc                \
+AR=$LFS_TGT-ar                 \
+RANLIB=$LFS_TGT-ranlib         \
+../configure                   \
+    --prefix=/tools            \
+    --disable-nls              \
+    --disable-werror           \
+    --with-lib-path=/tools/lib \
+    --with-sysroot
 
-# make -j$BUILD_JOBS $MAKE_FLAGS
-# make -j$BUILD_JOBS $MAKE_FLAGS install
+make -j$BUILD_JOBS $MAKE_FLAGS
+make -j$BUILD_JOBS $MAKE_FLAGS install
 
-# make -C ld clean
-# make -C ld LIB_PATH=/usr/lib:/lib
+make -C ld clean
+make -C ld LIB_PATH=/usr/lib:/lib
 
-# cp -v ld/ld-new /tools/bin
+cp -v ld/ld-new /tools/bin
 
-# EOF
+EOF
 
-# # GCC-8.2.0 - Pass 2 
+# GCC-8.2.0 - Pass 2 
 
-# install_package gcc-8.2.0.tar.xz 3 << EOF
+install_package gcc-8.2.0.tar.xz 3 << EOF
 
-# rm -rf build
+rm -rf build
 
-# cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
-#   `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include-fixed/limits.h
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include-fixed/limits.h
 
-# for file in gcc/config/{linux,i386/linux{,64}}.h
-# do
-#   cp -uv \$file{,.orig}
-#   sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
-#       -e 's@/usr@/tools@g' \$file.orig > \$file
-#   echo '
-# #undef STANDARD_STARTFILE_PREFIX_1
-# #undef STANDARD_STARTFILE_PREFIX_2
-# #define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
-# #define STANDARD_STARTFILE_PREFIX_2 ""' >> \$file
-#   touch \$file.orig
-# done
+for file in gcc/config/{linux,i386/linux{,64}}.h
+do
+  cp -uv \$file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+      -e 's@/usr@/tools@g' \$file.orig > \$file
+  echo '
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+#define STANDARD_STARTFILE_PREFIX_2 ""' >> \$file
+  touch \$file.orig
+done
 
-# case $(uname -m) in
-#   x86_64)
-#     sed -e '/m64=/s/lib64/lib/' \
-#         -i.orig gcc/config/i386/t-linux64
-#   ;;
-# esac
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+  ;;
+esac
 
-# tar -xf $SRC_DIR/mpfr-4.0.2.tar.xz
-# mv -v mpfr-4.0.2 mpfr
-# tar -xf $SRC_DIR/gmp-6.1.2.tar.xz
-# mv -v gmp-6.1.2 gmp
-# tar -xf $SRC_DIR/mpc-1.1.0.tar.gz
-# mv -v mpc-1.1.0 mpc
+tar -xf $SRC_DIR/mpfr-4.0.2.tar.xz
+mv -v mpfr-4.0.2 mpfr
+tar -xf $SRC_DIR/gmp-6.1.2.tar.xz
+mv -v gmp-6.1.2 gmp
+tar -xf $SRC_DIR/mpc-1.1.0.tar.gz
+mv -v mpc-1.1.0 mpc
 
-# mkdir -v build
-# cd       build
+mkdir -v build
+cd       build
 
-# CC=$LFS_TGT-gcc                                    \
-# CXX=$LFS_TGT-g++                                   \
-# AR=$LFS_TGT-ar                                     \
-# RANLIB=$LFS_TGT-ranlib                             \
-# ../configure                                       \
-#     --prefix=/tools                                \
-#     --with-local-prefix=/tools                     \
-#     --with-native-system-header-dir=/tools/include \
-#     --enable-languages=c,c++                       \
-#     --disable-libstdcxx-pch                        \
-#     --disable-multilib                             \
-#     --disable-bootstrap                            \
-#     --disable-libgomp
+CC=$LFS_TGT-gcc                                    \
+CXX=$LFS_TGT-g++                                   \
+AR=$LFS_TGT-ar                                     \
+RANLIB=$LFS_TGT-ranlib                             \
+../configure                                       \
+    --prefix=/tools                                \
+    --with-local-prefix=/tools                     \
+    --with-native-system-header-dir=/tools/include \
+    --enable-languages=c,c++                       \
+    --disable-libstdcxx-pch                        \
+    --disable-multilib                             \
+    --disable-bootstrap                            \
+    --disable-libgomp
 
-# make -j$BUILD_JOBS $MAKE_FLAGS
-# make -j$BUILD_JOBS $MAKE_FLAGS install
+make -j$BUILD_JOBS $MAKE_FLAGS
+make -j$BUILD_JOBS $MAKE_FLAGS install
 
-# ln -sv gcc /tools/bin/cc
+ln -sv gcc /tools/bin/cc
 
-# EOF
+EOF
